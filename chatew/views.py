80d3b3ad3c1d0676e66d909d4d5e24830d4333dd
devendran036 <69075292+devendran036@users.noreply.github.com>
2021-08-02 17:10:25 +0530

    Update views.py

--- chatew/views.py
+++ chatew/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from .forms import CreateUserForm
+from chatew.forms import CreateUserForm
 from django.contrib.auth.models import User
 def home(request):
     return render(request,'chat.html')
