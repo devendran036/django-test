82f7560e1c44e8e5ab132228a2053b835af77928
devendran036 <69075292+devendran036@users.noreply.github.com>
2021-08-03 14:38:08 +0530

    Update settings.py

--- devpychatupdated/settings.py
+++ devpychatupdated/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = ['djangosockettest.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'post',
     'friendrequest',
     
@@ -40,12 +47,6 @@ INSTALLED_APPS = [
     
     'register',
     'rest_framework',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     
     
 ]
